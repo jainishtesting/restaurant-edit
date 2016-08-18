12895fb2caea3dd6b296a753f7066851f7d71cb7
dhrumil162 <sanghaniuday007@gmail.com>
2016-08-18 23:35:10 +0530

    Update Resturent.aspx.cs

--- source/Resturent.aspx.cs
+++ source/Resturent.aspx.cs
@@ -105,6 +105,34 @@ public partial class Resturent : System.Web.UI.Page
         return json;
     }
     [WebMethod]
+    public static string updateHighlights(string highlights, int restauId)
+    {
+        if (!string.IsNullOrEmpty(highlights))
+        {
+            string[] highlights1 = highlights.Split(',');
+            List<SqlParameter> delparam = new List<SqlParameter>();
+            SqlParameter restauIdd = new SqlParameter("@restauId", restauId);
+            delparam.Add(restauIdd);
+            DataAccessCls ddlObj = new DataAccessCls();
+            ddlObj.RunSP_ReturnDT("pmstDeleteHighlightsByRestauId", delparam, System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+
+            foreach (var highlight in highlights1)
+            {
+                List<SqlParameter> highparam = new List<SqlParameter>();
+                SqlParameter hId = new SqlParameter("@RestaurantId", restauId);
+                SqlParameter hhId = new SqlParameter("@HighlightId", highlight);
+                List<SqlParameter> delparam1 = new List<SqlParameter>();
+                delparam1.Add(hId);
+                delparam1.Add(hhId);
+
+                DataAccessCls ddlObj1 = new DataAccessCls();
+                ddlObj1.RunSP_ReturnDT("pInsertHighlights", delparam1, System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            }
+        }
+        string json = "success";
+        return json;
+    }
+    [WebMethod]
     public static string getRestaurants(string key, int cityid, DateTime? fromdate, DateTime? todate)
     {
         List<SqlParameter> param = new List<SqlParameter>();
@@ -122,6 +150,26 @@ public partial class Resturent : System.Web.UI.Page
         string json = JsonConvert.SerializeObject(dt);
         return json;
     }
+
+    [WebMethod]
+    public static string updateValues(string fieldname, string value, int id)
+    {
+        List<SqlParameter> param = new List<SqlParameter>();
+        SqlParameter pkey = new SqlParameter("@fieldname", fieldname);
+        SqlParameter pcityid = new SqlParameter("@value", value);
+        SqlParameter pfromdate = new SqlParameter("@Id", id);
+        SqlParameter ptodate = new SqlParameter("@LinkMain", HttpContext.Current.Request.Url + "?LinkId=" + id);
+        param.Add(pkey);
+        param.Add(pcityid);
+        param.Add(pfromdate);
+        param.Add(ptodate);
+
+        DataAccessCls dObj = new DataAccessCls();
+        DataTable dt = dObj.RunSP_ReturnDT("sp_UpdateFields", param, ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+        string json = JsonConvert.SerializeObject(dt);
+        return json;
+    }
+
     [WebMethod]
     public static string toggleActive(int id, bool active)
     {
@@ -162,7 +210,7 @@ public partial class Resturent : System.Web.UI.Page
         {
             if (System.IO.Directory.Exists(HttpContext.Current.Server.MapPath("~/Upload/RestauMenus/") + id + "/"))
             {
-                System.IO.Directory.Delete(HttpContext.Current.Server.MapPath("~/Upload/RestauMenus/") + id + "/",true);
+                System.IO.Directory.Delete(HttpContext.Current.Server.MapPath("~/Upload/RestauMenus/") + id + "/", true);
             }
             if (System.IO.Directory.Exists(HttpContext.Current.Server.MapPath("~/Upload/RestauDocs/") + id + "/"))
             {
